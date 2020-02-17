8dbbd46b7e2b6c2fc8b646964831bffcd0b7083e
Phillip Kellogg <phillipkellogg@localhost.localdomain>
2020-02-16 22:01:45 -0500

    No Diving Deeper : (

--- sprint-1/src/components/mainVideo/MainVideo.jsx
+++ sprint-1/src/components/mainVideo/MainVideo.jsx
@@ -66,6 +66,7 @@ const Description = props => {
     </article>
   );
 };
+
 export default class Header extends React.Component {
   constructor() {
     super();
@@ -87,21 +88,21 @@ export default class Header extends React.Component {
             {
               id: 678,
               name: "Micheal Lyons",
-              timestamp: 26577,
+              timestamp: "12/18/2018",
               comment:
                 "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
             },
             {
               id: 876,
               name: "Gary Wong",
-              timestamp: 705904170,
+              timestamp: "12/18/2018",
               comment:
                 "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
             },
             {
               id: 777,
               name: "Theodore Duncan",
-              timestamp: 81905906540,
+              timestamp: "11/15/2018",
               comment:
                 "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
             }
