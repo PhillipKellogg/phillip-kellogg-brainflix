6e5524d0761019ed644f12946924a65e295b9a70
Phillip Kellogg <phillipkellogg@localhost.localdomain>
2020-02-16 22:34:23 -0500

    Workling Dive

--- sprint-1/src/components/mainVideo/MainVideo.jsx
+++ sprint-1/src/components/mainVideo/MainVideo.jsx
@@ -67,6 +67,38 @@ const Description = props => {
   );
 };
 
+function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+
+  var interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + " years ago";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months ago";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days ago";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours ago";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+}
+
+let date1 = 232224324;
+let date2 = 235466;
+let date3 = 1246444;
+let date4 = 124644224;
+
 export default class Header extends React.Component {
   constructor() {
     super();
@@ -83,26 +115,26 @@ export default class Header extends React.Component {
           likes: "110,985",
           duration: "0:42",
           video: Video,
-          timestamp: "12/18/2018",
+          timestamp: timeSince(new Date(Date.now() - date1)),
           comments: [
             {
               id: 678,
               name: "Micheal Lyons",
-              timestamp: "12/18/2018",
+              timestamp: timeSince(new Date(Date.now() - date2)),
               comment:
                 "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
             },
             {
               id: 876,
               name: "Gary Wong",
-              timestamp: "12/18/2018",
+              timestamp: timeSince(new Date(Date.now() - date3)),
               comment:
                 "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
             },
             {
               id: 777,
               name: "Theodore Duncan",
-              timestamp: "11/15/2018",
+              timestamp: timeSince(new Date(Date.now() - date4)),
               comment:
                 "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
             }
